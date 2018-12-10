a94126e0d5c92d7ffe17bcb2c44bd046066cd32a
Trevor Ford <info@trvrfrd.net>
2018-12-10 18:44:39 -0500

    day 2: rename BoxID to Box, refactor a couple things

--- 2/solutions.rb
+++ 2/solutions.rb
@@ -8,56 +8,60 @@
 ################
 
 # what if I did things... the OOP way?!
-class BoxID
+class Box
   attr_reader :id
 
-  def initialize id_str
-    @id = id_str
+  def initialize id
+    @id = id
   end
 
   def letter_counts
-    return @counts if @counts
+    return @letter_counts if @letter_counts
 
-    @counts = Hash.new(0)
-    id.each_char { |c| @counts[c] += 1 }
-    @counts
+    @letter_counts = Hash.new(0)
+    id.each_char { |c| @letter_counts[c] += 1 }
+    @letter_counts
   end
 
   def letter_repeated_n_times? n
     letter_counts.values.any? { |count| count == n }
   end
 
-  def differ_by_one_letter? other_box_id
+  def differ_by_one_letter? other_box
     differing_letter_counts = 0
+
     (0...id.length).each do |idx|
-      differing_letter_counts += 1 if id[idx] != other_box_id.id[idx]
+      differing_letter_counts += 1 if id[idx] != other_box.id[idx]
       return false if differing_letter_counts > 1
     end
 
     true
   end
 
-  def letters_in_common_with other_box_id
+  def letters_in_common_with other_box
     common_letters = id
-    id.each_char.with_index do |_, idx|
-      if id[idx] != other_box_id.id[idx]
-        common_letters[idx] = ""
+
+    (0...id.length).each do |idx|
+      if id[idx] != other_box.id[idx]
+        # delete spaces later, don't want to mess up the index by deleting now
+        common_letters[idx] = " "
         break
       end
     end
-    common_letters
+
+    common_letters.gsub " ", ""
   end
 end
 
 input_path = File.expand_path("input.txt", File.dirname(__FILE__))
-box_ids = File.read(input_path).split("\n").map { |id_str| BoxID.new id_str }
+boxes = File.read(input_path).split("\n").map { |id_str| Box.new id_str }
 
 double_letter_count = 0
 triple_letter_count = 0
 
-box_ids.each do |id|
-  double_letter_count += 1 if id.letter_repeated_n_times? 2
-  triple_letter_count += 1 if id.letter_repeated_n_times? 3
+boxes.each do |box|
+  double_letter_count += 1 if box.letter_repeated_n_times? 2
+  triple_letter_count += 1 if box.letter_repeated_n_times? 3
 end
 
 checksum = double_letter_count * triple_letter_count
@@ -76,9 +80,9 @@ letters_in_common = ""
 # too bad I don't know anything so I did it the brute force way :(
 # but hey Array#permutaion is pretty fun and smart
 # and look at that (block argument destructuring)!
-box_ids.permutation(2).each do |(id1, id2)|
-  if id1.differ_by_one_letter? id2
-    letters_in_common = id1.letters_in_common_with id2
+boxes.permutation(2).each do |(box1, box2)|
+  if box1.differ_by_one_letter? box2
+    letters_in_common = box1.letters_in_common_with box2
     break
   end
 end
