2c226bf18c3b488a6c91eff070afb6ff7c5d3671
Trevor Ford <info@trvrfrd.net>
2018-12-09 21:20:13 -0500

    git init + add README + solve day 1

---
+++ 1/solutions.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# solutions to part 1 and 2 of:
+# https://adventofcode.com/2018/day/1
+
+##################
+# begin part 1 #
+##################
+
+# make sure we get the path relative to the running script (this file)
+# otherwise if we're executing the script from project_root/ vs project_root/1/
+# we'll get different paths and everything will explode
+input_path = File.expand_path("input.txt", File.dirname(__FILE__))
+frequency_changes = File.read(input_path).split("\n").map(&:to_i)
+initial_frequency = 0
+
+resulting_frequency = frequency_changes.reduce(initial_frequency, &:+)
+
+puts "Day 1 Part 1 solution:", resulting_frequency
+
+
+##################
+# begin part 2 #
+##################
+
+require "set"
+
+frequencies_reached = Set.new
+frequencies_reached.add(initial_frequency)
+
+# this could run forever but the puzzle seems to guarantee a repeat frequency
+frequency_changes.cycle.reduce(initial_frequency) do |current_frequency, change|
+  current_frequency += change
+  if frequencies_reached.include?(current_frequency)
+    puts "Day 1 Part 2 solution:", current_frequency
+    break
+  end
+  frequencies_reached.add(current_frequency)
+  current_frequency
+end
---
+++ README.md
@@ -0,0 +1,5 @@
+these are my solutions for [Advent of Code 2018](https://adventofcode.com/2018)
+in Ruby even though I already know Ruby because I want to review Ruby
+
+the solutions are all executable, e.g. `$ ./1/solutions.rb`
+they will use `/usr/bin/env ruby' so hopefully you've got that set up :O
